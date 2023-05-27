a91531117a7635f05e862fddb520865680b1c49c
jieun <jieunc023@gmail.com>
2023-05-27 22:14:13 +0900

    naverlogin, UI

--- src/main/java/com/example/webproject/LoginController.java
+++ src/main/java/com/example/webproject/LoginController.java
@@ -1,22 +1,24 @@
 package com.example.webproject;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.Map;
 
-@Controller
+@org.springframework.stereotype.Controller
 @RequestMapping("/oauth")
-public class KaKaoController {
+public class LoginController {
 
     @Autowired
     KaKaoService ks;
+    @Autowired
+    NaverService ns;
 
     @GetMapping("/login")
     public String loginPage()
@@ -28,10 +30,24 @@ public class KaKaoController {
      * [GET] /oauth/kakao/callback
      */
     @GetMapping("/kakao")
-    public String getCI(@RequestParam String code, Model model) throws IOException {
+    public String getKakao(@RequestParam String code, Model model) throws IOException {
         System.out.println("code = " + code);
         String access_token = ks.getKakaoAccessToken(code);
         Map<String, Object> userInfo = ks.getUserInfo(access_token);
+        model.addAttribute("code", code);
+        model.addAttribute("access_token", access_token);
+        model.addAttribute("userInfo", userInfo);
+        //ci는 비즈니스 전환후 검수신청 -> 허락받아야 수집 가능
+        return "index";
+    }
+
+    @GetMapping("/naver")
+    public String getNaver(@RequestParam String code, Model model) throws IOException {
+        System.out.println("code = " + code);
+        SecureRandom random = new SecureRandom();
+        String stateString = new BigInteger(130, random).toString(32);
+        String access_token = ns.getNaverAccessToken(code, stateString);
+        Map<String, Object> userInfo = ns.getNaverUserInfo(access_token);
         model.addAttribute("code", code);
         model.addAttribute("access_token", access_token);
         model.addAttribute("userInfo", userInfo);

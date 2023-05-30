c092ed55d6486a536209d99e779a503f16a32a01
milkioiy <93613157+milkioiy@users.noreply.github.com>
2023-05-30 17:16:31 +0900

    Update index.html
    Update index.html

--- src/main/java/com/example/webproject/controller/HelloController.java
+++ src/main/java/com/example/webproject/controller/HelloController.java
@@ -6,6 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
     @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/index")
     public String index() {
         return "index";
     }

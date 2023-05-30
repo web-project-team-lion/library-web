08a017b81b43cdeadfa49c15bbaafde2b16f3310
milkioiy <93613157+milkioiy@users.noreply.github.com>
2023-05-30 14:20:20 +0900

    Revert "Merge pull request #9 from web-project-team-lion/milkioiy_board_create"
    This reverts commit 5a303311d20e2aad4f888a3b45ae10a4f3dfc76b, reversing
    changes made to e4e8352af441896104073c81b7e70499eeb8d7d3.

--- src/main/java/com/example/webproject/controller/HelloController.java
+++ src/main/java/com/example/webproject/controller/HelloController.java
@@ -2,13 +2,10 @@ package com.example.webproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class HelloController {
-    @GetMapping()
+    @GetMapping("/")
     public String index() {
         return "index";
     }
--- src/main/java/com/example/webproject/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.webproject.repository;
-
-import com.example.webproject.data.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}

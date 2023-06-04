012564d2ce8afa4e029718527d8b3e548b9e419c
LeeJina <97230811+cocojina@users.noreply.github.com>
2023-06-04 15:40:56 +0900

    fix board.html, add boardDetail.html, add boardForm.html

--- src/main/java/com/example/webproject/controller/HelloController.java
+++ src/main/java/com/example/webproject/controller/HelloController.java
@@ -3,13 +3,26 @@ package com.example.webproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class HelloController {
-    @GetMapping()
+    @GetMapping("/index")
     public String index() {
         return "index";
     }
+
+    @GetMapping("/home")
+    public String home() {return "home";}
+
+    @GetMapping("/login")
+    public String login() {return "login";}
+
+    @RequestMapping("/board")
+    public String board() {return "board";}
+
+    @RequestMapping("/boardForm")
+    public String boardForm() {return "boardForm";}
+
+    @RequestMapping("/boardDetail")
+    public String boardDetail() {return "boardDetail";}
 }

71504ce379090ede4d498847584b0bc192f0a9b4
LeeJina <97230811+cocojina@users.noreply.github.com>
2023-06-04 15:50:57 +0900

    git fix

--- src/main/java/com/example/webproject/controller/HelloController.java
+++ src/main/java/com/example/webproject/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.webproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
@@ -18,11 +19,5 @@ public class HelloController {
     public String login() {return "login";}
 
     @RequestMapping("/board")
-    public String board() {return "board";}
-
-    @RequestMapping("/boardForm")
-    public String boardForm() {return "boardForm";}
-
-    @RequestMapping("/boardDetail")
-    public String boardDetail() {return "boardDetail";}
+    public String product() {return "board";}
 }
